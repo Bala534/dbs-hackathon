80ec5e47c9a5d1512b4ffdf90a9a01fd7d13c8df
Bala Mahesh <72618397+Bala534@users.noreply.github.com>
2022-10-29 21:51:19 +0530

    Delete config directory

--- config/view-helper.js
+++
@@ -1,9 +0,0 @@
-const env = require("./environment");
-
-module.exports = (app) => {
-    app.locals.assetPath = function (filePath) {
-        if (env.name == 'development') {
-            return '/' + filePath;
-        }
-    }
-}
