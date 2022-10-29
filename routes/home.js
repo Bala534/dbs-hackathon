e4b207b58f93fc77279c9484cd72c9590b25480d
Bala Mahesh <72618397+Bala534@users.noreply.github.com>
2022-10-29 21:53:05 +0530

    Delete routes directory

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const homeController = require('../controllers/home_controller');
-
-// pages
-router.get('/', passport.checkAuthentication, homeController.homePage);
-
-module.exports = router;
